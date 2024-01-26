3f3243526e6908b96ae56f4599ce5b474fffb8c7
Miracle Friday <miraclef60@gmail.com>
2024-01-26 18:45:55 +0100

    feat: made aside responsive and used a drawer in mobile view

--- src/components/main-layout.tsx
+++ src/components/main-layout.tsx
@@ -1,6 +1,16 @@
 import Image from "next/image";
 import React from "react";
 import profilePic from "../assets/profile-pic.jpeg";
+import { Menu, X } from "lucide-react";
+import { Button } from "~/components/ui/button";
+import {
+  Drawer,
+  DrawerContent,
+  DrawerDescription,
+  DrawerHeader,
+  DrawerClose,
+  DrawerTrigger,
+} from "./ui/drawer";
 
 const menuItems = [
   {
@@ -58,15 +68,83 @@ const socialsMenuItems = [
  * On the Resource menu should be a subtle clipboard outline with a minimalistic animating stars background
  * On the Socials menu should be an iPhone outline with an iPhone style gradient background, with the menu items layout as app icons
  */
+/**
+ * Logo font inspiration: Jomolhari, Renin, Kenia, Judson, KoPub Batang, Lily Script One, Lohit Tamil, Lobster, Milonga, Monofett, Mogra, Modern Antigua, Neonderthaw, Press Start 2p,
+ * Rozha One, Vesper Libre, Viaoda Libre, VT323.
+ */
 
 export const MainLayout: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => {
   return (
-    <main className="flex h-screen w-screen items-center justify-center">
-      <aside className="sticky bottom-0 left-0 top-0 hidden h-full max-w-lg items-start justify-start overflow-y-scroll bg-slate-800 pb-6 sm:grid sm:min-w-fit">
+    <main className="grid h-screen w-screen sm:flex sm:items-center sm:justify-center">
+      <header className="flex items-center justify-between border-b-[.3px] border-gray-200/20 px-8 py-6 sm:hidden">
+        <div className="flex w-full items-center justify-start space-x-4 rounded-xl shadow-sm">
+          <div className="shrink-0">
+            <Image
+              width={40}
+              height={40}
+              className="h-10 w-10 rounded-lg border border-slate-600"
+              src={profilePic}
+              alt="ChitChat Logo"
+            />
+          </div>
+          <div>
+            <div className="text-sm font-normal text-white">Miracle Friday</div>
+            {/* Animate this section to flip different skill sets */}
+            <p className="text-xs text-slate-300">Software Engineer</p>
+          </div>
+        </div>
+        <Drawer direction="left" dismissible={false}>
+          <DrawerTrigger asChild>
+            <Button
+              variant="outline"
+              className="rounded border-[.56px] border-gray-100 p-2"
+            >
+              <Menu color="gray" className="h-6 w-6 text-zinc-200" />
+            </Button>
+          </DrawerTrigger>
+          <DrawerContent
+            showIndicator={false}
+            className="h-full w-full"
+            draggable={false}
+          >
+            <div className="mx-auto w-full max-w-sm">
+              <DrawerHeader>
+                <div className="flex items-center justify-between border-b-[.3px] border-gray-200/20 px-4 py-4">
+                  <div className="flex w-full items-center justify-start space-x-4 rounded-xl shadow-sm">
+                    <div className="shrink-0">
+                      <Image
+                        width={40}
+                        height={40}
+                        className="h-10 w-10 rounded-lg border border-slate-600"
+                        src={profilePic}
+                        alt="ChitChat Logo"
+                      />
+                    </div>
+                    <div className="grid place-items-start">
+                      <div className="text-sm font-normal text-white">
+                        Miracle Friday
+                      </div>
+                      {/* Animate this section to flip different skill sets */}
+                      <p className="text-xs text-slate-300">
+                        Software Engineer
+                      </p>
+                    </div>
+                  </div>
+                  <DrawerClose>
+                    <X color="gray" className="h-6 w-6 text-zinc-200" />
+                  </DrawerClose>
+                </div>
+              </DrawerHeader>
+              <div className="p-4 pb-0"></div>
+            </div>
+          </DrawerContent>
+        </Drawer>
+      </header>
+      <aside className="sticky bottom-0 left-0 top-0 hidden h-full max-w-lg items-start justify-start overflow-y-scroll bg-zinc-900 pb-6 sm:grid sm:min-w-fit">
         <div className="grid min-w-[250px] items-start justify-stretch gap-1 px-2">
-          <div className="flex w-full items-center justify-start space-x-4 rounded-xl px-3 py-6 shadow-lg">
+          <div className="flex w-full items-center justify-start space-x-4 rounded-xl px-3 py-6 shadow-sm">
             <div className="shrink-0">
               <Image
                 width={40}
@@ -89,32 +167,36 @@ export const MainLayout: React.FC<{ children: React.ReactNode }> = ({
             <a
               href={item.href}
               key={item.title}
-              className="flex w-full items-start justify-start rounded-md px-4 py-2 text-sm font-light text-white/70 hover:bg-slate-300/20 hover:text-white/100"
+              className="flex w-full items-start justify-start rounded-md px-4 py-2 text-sm font-light text-white/70 hover:bg-zinc-500/10 hover:text-[#FF4A01]"
             >
               {item.icon}
               {item.title}
             </a>
           ))}
           <div className="my-2" />
-          <span className="px-4 text-xs text-slate-300/60">Resources</span>
+          <span className="px-4 text-xs font-extralight text-slate-300/60">
+            Resources
+          </span>
           {resourceMenuItems.map((item) => (
             <a
               href={item.href}
               key={item.title}
-              className="flex w-full items-start justify-start rounded-md px-4 py-2 text-sm font-light text-white/70 hover:bg-slate-300/20 hover:text-white/100"
+              className="flex w-full items-start justify-start rounded-md px-4 py-2 text-sm font-light text-white/70 hover:bg-zinc-500/10 hover:text-[#FF4A01]"
             >
               {item.icon}
               {item.title}
             </a>
           ))}
           <div className="my-2" />
-          <span className="px-4 text-xs text-slate-300/60">Socials</span>
+          <span className="px-4 text-xs font-extralight text-slate-300/60">
+            Socials
+          </span>
           {socialsMenuItems.map((item) => (
             <a
               href={item.href}
               key={item.title}
               target="_blank"
-              className="flex w-full items-start justify-start rounded-md px-4 py-2 text-sm font-light text-white/70 hover:bg-slate-300/20 hover:text-white/100"
+              className="flex w-full items-start justify-start rounded-md px-4 py-2 text-sm font-light text-white/70 hover:bg-zinc-500/10 hover:text-[#FF4A01]"
             >
               {item.icon}
               {item.title}
