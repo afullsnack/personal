ff61f98e2b88e2f6988dce416fb8e93691fe21fc
Miracle Friday <miraclef60@gmail.com>
2024-02-14 14:44:05 +0100

    feat: added forceTheme to dark mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,6 +30,7 @@ export default function RootLayout({
           attribute="class"
           defaultTheme="system"
           enableSystem
+          forcedTheme="dark"
           disableTransitionOnChange
         >
           <MainLayout>
