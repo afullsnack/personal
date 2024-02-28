323e5536e959ab9aae81ca21617f1d466adda5f6
Miracle Friday <miraclef60@gmail.com>
2024-02-28 02:08:54 +0100

    fix: fixed slug page for articles
    - Added a draft folder in the content directory that will be git ignored on production, and will hold all draft posts before publishing
    - Added the folder to .gitignore
    - Added more content to mdx file with my first post

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -38,13 +38,13 @@ export default function Page({ params }: PostProps) {
     <main className="flex min-h-screen flex-col items-center justify-start bg-gradient-to-br from-zinc-50 to-zinc-200 text-black dark:from-zinc-950 dark:to-zinc-900 dark:text-white">
       <div className="container mx-auto flex flex-col items-start justify-start gap-6 px-8 py-8 md:px-16 md:py-16 lg:px-32 lg:py-32">
         <div className="grid gap-2 lg:max-w-lg">
-          <h2 className="text-3xl font-bold">{post.title}</h2>
+          <h2 className="text-3xl font-bold text-slate-200">{post.title}</h2>
           <time dateTime={post.date} className="block text-xs text-gray-400">
             {format(parseISO(post.date), "LLLL d, yyyy")}
           </time>
           <h2 className="mt-1 text-sm">{post.author}</h2>
         </div>
-        <div className="grid flex-wrap gap-2 lg:max-w-lg">
+        <div className="grid flex-wrap gap-2 text-slate-300 lg:max-w-2xl">
           <MDXContent code={post.body.code} />
         </div>
       </div>
